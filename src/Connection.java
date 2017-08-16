43acd071c59bfa124d0c5b11f0706096b8e8735d
vfalkenberg@bk.ru <134679>
2017-08-16 20:11:06 +0600

    Сервер чата

---
+++ src/Connection.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Connection extends Thread {
+    private BufferedReader in;
+    private PrintWriter out;
+    private String name = "";
+    private Socket socket;
+    private static ArrayList<Connection> connections = new ArrayList<>();
+
+    public static ArrayList<Connection> getConnections() {
+        return connections;
+    }
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void run() {
+        try {
+            name = in.readLine();
+
+            for (Connection c : connections) {
+                c.out.println(name + " присоединился к чату");
+            }
+            String str = "";
+            while (true) {
+                str = in.readLine();
+                if (str.equals("exit")) break;
+
+                for (Connection c : connections) {
+                    c.out.println(name + ": " + str);
+                }
+            }
+
+            for (Connection c : connections) {
+                c.out.println(name + " покинул чат.");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket server;
+
+    public Server() throws IOException {
+        try {
+            server = new ServerSocket(Config.PORT);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                Socket socket = server.accept();
+
+                Connection con = new Connection(socket);
+                Connection.getConnections().add(con);
+                con.start();
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            server.close();
+        }
+    }
+}
