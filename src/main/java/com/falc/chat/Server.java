2a4a7f219f4b03aa3d1196379b41e6ebb58715c5
vfalkenberg@bk.ru <134679>
2017-08-27 21:08:52 +0600

    Добавлен pool потоков

--- src/main/java/com/falc/chat/Server.java
+++ src/main/java/com/falc/chat/Server.java
@@ -1,38 +1,80 @@
 package com.falc.chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     private ServerSocket server;
+    private ExecutorService service = Executors.newCachedThreadPool();
+    private ConcurrentHashMap<String, PrintWriter> mapClients = new ConcurrentHashMap<>();
+
 
     public Server() throws IOException {
 
     }
-    public void createServerSoket(){
+
+    public void createServerSoket() {
         try {
             server = new ServerSocket(Config.PORT);
+            System.out.println("Сервер запущен");
         } catch (IOException e) {
             System.out.println("Ошибка создания сокета");
         }
-        System.out.println("Сервер запущен");
+
     }
+
     public void startServer() throws IOException {
         try {
             while (true) {
                 Socket socket = server.accept();
 
-                Connection con = new Connection(socket);
-                Connection.getConnections().add(con);
-                con.start();
+                service.submit(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+
+                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                            String name = in.readLine();
+                            mapClients.putIfAbsent(name,out);
 
 
+                            for (Map.Entry<String, PrintWriter> pair : mapClients.entrySet()) {
+                                pair.getValue().println(name + " присоединился к чату");
+                            }
+                            String str = "";
+                            while (true) {
+                                str = in.readLine();
+                                if (str.equals("exit")) break;
+
+                                for (Map.Entry<String, PrintWriter> pair : mapClients.entrySet()) {
+                                    pair.getValue().println(name + ": " + str);
+                                }
+                            }
+
+                            for (Map.Entry<String, PrintWriter> pair : mapClients.entrySet()) {
+                                pair.getValue().println(name + " покинул чат.");
+                                mapClients.remove(name);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         } finally {
             server.close();
+            service.shutdown();
         }
     }
 }
